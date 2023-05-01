ddef73e7ccc7eb7da4b4dd3fdc8bfd8b35dc31d0
Shams425 <alexdavid486@gmail.com>
2023-05-01 09:14:12 +0200

    making the very first hello world and applying the styles to it

---
+++ src/pages/index.jsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        color: "black",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h2>Hello world</h2>
+    </div>
+  );
+}
