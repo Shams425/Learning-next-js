65e744e803ebb45b8289e2522a8c48113b6cb8bd
Shams425 <alexdavid486@gmail.com>
2023-05-07 10:30:22 +0200

    making the css module

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import "@/styles/product.module.css";
 
 function Products() {
   return (
     <>
-      <h2>The Products Home Page</h2>
+      <h2 className="title">The Products Home Page</h2>
       <Link href="/products/1">product 1</Link>
       <Link href="/products/2">product 2</Link>
       <Link href="/products/3">product 3</Link>
