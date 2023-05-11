a97c5f179c6e54aca9566a0748ca48a1827d1381
Shams425 <alexdavid486@gmail.com>
2023-05-11 22:02:51 +0200

    making the app layout concept

---
+++ components/header/header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="headerContainer">
+      <h1>header</h1>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />;
+      <Footer />
+    </>
+  );
 }
